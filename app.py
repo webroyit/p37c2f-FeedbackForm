d34d5e4a2ea9552c874996b87b62ef958255105e
royweb <xytank@gmail.com>
2020-02-07 15:21:17 -0500

    Add a route to get the feedback data from the client

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# define the route
-@app.route("/")
-
 # show the home page
+@app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    if request.method == "POST":
+        customer = request.form["customer"]
+        dealer = request.form["dealer"]
+        rating = request.form["rating"]
+        comments = request.form["comments"]
+        
+        return render_template("success.html")
 
 if __name__ == "__main__":
     # allow the server to keep reloading
