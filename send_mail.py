6a22dac119dedb6b3daa638b6f17babb268b3f85
royweb <xytank@gmail.com>
2020-03-21 14:24:48 -0400

    Add config.py

--- send_mail.py
+++ send_mail.py
@@ -1,6 +1,12 @@
 import smtplib
 from email.mime.text import MINEText
 
+from config import USERNAME, PASSWORD
+
 def send_mail(customer, dealer, rating, comments):
     port = 2525
     smtp_server = "smtp.mailtrap.io"
+
+    # credentials from mailtrap.io
+    login = USERNAME
+    password = PASSWORD
