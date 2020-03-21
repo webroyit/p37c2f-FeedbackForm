ebe1bcca72abe058de46aaacb27f87e41a8d0ab7
royweb <xytank@gmail.com>
2020-03-21 15:25:40 -0400

    Set up the format of the email

--- send_mail.py
+++ send_mail.py
@@ -1,5 +1,5 @@
 import smtplib
-from email.mime.text import MINEText
+from email.mime.text import MIMEText
 
 from config import USERNAME, PASSWORD
 
@@ -10,3 +10,14 @@ def send_mail(customer, dealer, rating, comments):
     # credentials from mailtrap.io
     login = USERNAME
     password = PASSWORD
+
+    # message of the email
+    message = f"<h3>Feedback</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comment: {comments}</li></ul>"
+
+    # set up the content of the email
+    sender_email = "wriboat1@mail.com"
+    receiver_email = "wriboat2@mail.com"
+    msg = MIMEText(message, "html")
+    msg["Subject"] = "WRIboat Feedback"
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
